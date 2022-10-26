5d527c2dfd65534f10f79039088fdad27a4dcc76
Maximilian <maximilian.hart@inrae.fr>
2022-10-26 22:54:32 +0200

    ajout service pour mettre à jour champs asterix

--- lib/api/mobile_api.dart
+++ lib/api/mobile_api.dart
@@ -1262,4 +1262,64 @@ class MobileApi {
       return null;
     }
   }
+  /// Update a section&#39;s asterix fields
+  ///
+  ///
+  Future<dynamic> updateSectionAsterixs(String uri,  { List<String> asterixFieldNames, String acceptLanguage }) async {
+    Object postBody = null;
+
+    // verify required params are set
+    String authorization = apiClient.token;
+    if(uri == null) {
+      throw new ApiException(400, "Missing required param: uri");
+    }
+    if(authorization == null) {
+      throw new ApiException(400, "First connect with connectToOpenSILEX function");
+    }
+
+    // create path and map variables
+    String path = "/mobile/update_section_asterix/{uri}".replaceAll("{format}","json").replaceAll("{" + "uri" + "}", uri.toString());
+
+    // query params
+    List<QueryParam> queryParams = [];
+    Map<String, String> headerParams = {};
+    Map<String, String> formParams = {};
+    if(asterixFieldNames != null) {
+      queryParams.addAll(_convertParametersForCollectionFormat("multi", "asterixFieldNames", asterixFieldNames));
+    }
+    headerParams["Authorization"] = authorization;
+    headerParams["Accept-Language"] = acceptLanguage;
+
+    List<String> contentTypes = ["application/json"];
+
+    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
+    List<String> authNames = [];
+
+    if(contentType.startsWith("multipart/form-data")) {
+      bool hasFields = false;
+      MultipartRequest mp = new MultipartRequest(null, null);
+
+      if(hasFields)
+        postBody = mp;
+    }
+    else {
+    }
+
+    var response = await apiClient.invokeAPI(path,
+        'PUT',
+        queryParams,
+        postBody,
+        headerParams,
+        formParams,
+        contentType,
+        authNames);
+
+    if(response.statusCode >= 400) {
+      throw new ApiException(response.statusCode, response.body);
+    } else if(response.body != null) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
 }
